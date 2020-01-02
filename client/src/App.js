aab5d358591e38d78df8d5c407ba6191f1499c1e
Jeremiah Stafford <52318268+jstafford74@users.noreply.github.com>
2020-01-02 04:56:42 -0500

    local image store

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,15 @@ import Score from './components/Score'
 class App extends Component {
   state = {
     tiles: [
-      'https://vignette.wikia.nocookie.net/monstertruck/images/0/0a/AUT-14-RK1739-01P.JPG/revision/latest/scale-to-width-down/185?cb=20160104024211',
-      'https://vignette.wikia.nocookie.net/monstertruck/images/e/ea/Shattered.jpg/revision/latest/scale-to-width-down/185?cb=20150206013820',
-      'https://vignette.wikia.nocookie.net/monstertruck/images/8/82/Truck-1.jpg/revision/latest/scale-to-width-down/185?cb=2015082018115',
-      'https://vignette.wikia.nocookie.net/monstertruck/images/6/6e/21232026_10154918738928193_26059027075219651_n.jpg/revision/latest/scale-to-width-down/185?cb=20170903214753',
-      'http://www.chinonthetank.com/wp-content/uploads/2013/04/1280px-Monster_mutt_truck-150x150.jpg',
-      'http://www.chinonthetank.com/wp-content/uploads/2013/04/1280px-Monster_mutt_truck-150x150.jpg',
-      'https://www.rccaraction.com/wp-content/uploads/2014/08/KBHPClod-180x180.jpg',
-      'https://games.torrentsnack.com/wp-content/uploads/2012/11/Monster-Jam-Path-of-Destruction-PS3-150x150.jpg',
-      'https://carsintrend.com/wp-content/uploads/2017/10/2018-ford-f-250-features-150x150.jpg'
+      '/img/BigFootBeach.jpg',
+      '/img/BogFoot.jpg',
+      '/img/Dragon.jpg',
+      '/img/Dragonice.jpg',
+      '/img/GraveDigger.jpg',
+      '/img/OldSchoolGD.jpg',
+      '/img/reddragon.jpg',
+      '/img/Scooby-Doo-Air.jpg',
+      'Zombie.jpg'
     ],
     score: 0,
     topScore: 0
